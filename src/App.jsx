9bf750067ce599c872e3d9521fb689e995081b42
Jose Marin <jlma.ing@protonmail.com>
2022-06-11 16:41:22 -0500

    react-router 6 added and initial configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Fragment } from "react";
-import movies from "./Film.json";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import { MovieGrid } from "./MovieGrid";
+import { MovieDetails } from "./MovieDetails";
 import styles from "./App.module.css";
+import movies from "./Film.json";
 
 
 export function App() {
     console.log(movies);
     return (
-        <Fragment>
+        <Router>
             <header>
-                <h1 className={styles.tittle}>
-                    Movies
-                </h1>
+                <Link replace="true" to="/">
+                    <h1 className={styles.tittle}>
+                        Movies
+                    </h1>
+                </Link>
             </header>
             <main>
-                <MovieGrid />
+                <Routes>
+                    <Route path="/details/:movieId" element={<MovieDetails />} />
+                    <Route path="/" element={<MovieGrid />} />
+                </Routes>
             </main>
-        </Fragment>
+        </Router>
     );
 }
