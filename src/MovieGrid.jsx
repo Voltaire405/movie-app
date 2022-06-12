ad2da99ca880eb5969f1a53e1c673ff55d4c23cc
Jose Marin <jlma.ing@protonmail.com>
2022-06-12 10:08:33 -0500

    add spinner loading

--- src/MovieGrid.jsx
+++ src/MovieGrid.jsx
@@ -4,17 +4,25 @@ import { MovieCard } from "./MovieCard";
 
 import { get } from "./utils/HttpRequests";
 import styles from "./MovieGrid.module.css";
+import { Spinner } from "./components/Spinner";
 
 export function MovieGrid() {
 
+    const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
 
     useEffect(
         () => {
             get("/discover/movie")
-                .then((data) => setMovies(data.results));
+                .then((data) => {
+                    setMovies(data.results);
+                    setLoading(false);
+                });
         }, []);
 
+    if (loading) {
+        return <Spinner />
+    }
     return (
         <ul className={styles.movieGrid} >
             {movies.map((movie) => (<MovieCard key={movie.id} movie={movie} />))}
